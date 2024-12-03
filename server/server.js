288836901133b99110e5a697ad0be3f8d5959976
Matthew West <Mwest1310@outlook.com>
2024-12-03 09:44:46 +0000

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -28,13 +28,13 @@ app.use('/api/cart', cartRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-if(process.env.NODE_ENV === 'production'){
+/*if(process.env.NODE_ENV === 'production'){
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, 'client/dist')));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
 } else {
     app.get('/', (req, res) => res.send('Server is ready'));
-};
+};*/
 // connects to database then runs server
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
